5ff9ae489cc7d31892fb02ff141a4c855d4c9e85
Matthew Connelly <github@maff.im>
2015-09-06 01:46:08 +0100

    Fixed windows notification toast integration to be more compliant. Still requires an external program, but one that can be easily downloaded from a reliable source, and be used as standard. Also added a build script for windows, to create a standalone exe.

---
+++ winbuild.bat
@@ -0,0 +1,16 @@
+pp -B -c -S -z 9 ^
+-cd ayudante-lobo.pp-deps ^
+-o ayudante-lobo.exe ^
+-M Cwd ^
+-M Date::Format ^
+-M Net::SSH2 ^
+-M POE::Component::DirWatch::WithCaller ^
+-M POSIX ^
+-M Speech::Synthesis ^
+-M Try::Tiny ^
+-M Unix::PID ^
+-M Win32::API ^
+-M Win32::Clipboard ^
+-M Win32::SAPI5 ^
+-M YAML ^
+ayudante-lobo
